1b6ab11e0693e31f2f0b2d09c9af0f1bc53043b5
Chris <117885718+ChristBernardS@users.noreply.github.com>
2024-06-19 23:30:35 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,12 @@
+'''
+Dimas adalah seorang seniman. Ia ingin membuat mahakarya dengan sentuhan yang berbeda.
+Anda adalah teman dekat Dimas, tugas anda adalah membantu Dimas mengerjakan mahakarya-
+tersebut. Silahkan berkreasi sesuka hati anda dalam membuat mahakarya tersebut. Perlu
+diingat bahwa tugas ini tetap ada pengecekan plagiasi, apabila anda terkena plagiasi
+maka nilai yang didapat adalah 0.
+'''
+
+with open ("art.txt", "w") as file:
+    pass
+    # code anda disini
+
